d932fae48a554a699953428eaa7b9f7965ffda9a
simachewh <simachew.tibebu@gmail.com>
2016-07-29 01:10:18 +0300

    validation length and dash position.

--- validateSSN.js
+++ validateSSN.js
@@ -1,5 +1,8 @@
 /**
  * Created by Simachew on 28-Jul-16.
+ *
+ * A router to handle validation of SSN is defined in this file.
+ * This router should be registered by the respective app.
  */
 
 var express = require("express");
@@ -10,19 +13,33 @@ router.route("/")
     .get(function (req, res) {
         if(req.query.ssn){
             var ssn = req.query.ssn;
+            res.header("Content-Type", "text/plain");
 
-            if(ssn.length == 11 && ssn.indexOf("-") !== -1){
-                res.header("Content-Type", "text/plain");
-                res.status(200).end(ssn + " true");
+            if(isSSNValid(ssn)){
+                res.status(200).end("true");
             }
             else
             {
-                res.header("Content-Type", "text/plain");
-                res.status(400).end(ssn + " false");
+                res.status(200).end("false");
             }
+        }else{
+            res.status(400).end();
         }
 
 
+    })
+    .post(function (req, res) {
+        res.header("Content-Type", "text/plain");
+        res.status(405).end("POST not allowed");
     });
+
+function isSSNValid(ssn) {
+    if(ssn.length == 11
+        && ssn.indexOf("-") == 6 ){
+        return true;
+    }else{
+        return false;
+    }
+};
 
 module.exports = router;
